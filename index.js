f6837178f672d5a259ba58f405e138eb06503859
Sean Hoar <sean.hoar@gmail.com>
2016-10-27 09:07:36 -0400

    Init Guestbook - File Structure

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var hbs = require('express-handlebars')
+var mongoose = require('mongoose')
+
+var app = express()
+
+app.engine('handlebars', hbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+// get guestbook page with existing comments
+// app.get('/comments')
+// app.post('/comments/:id')
+// app.get('/comments/:id')
+// app.get('/comments/new')
+// app.post('/comments/new')
+
+
+
+app.listen(3000)
